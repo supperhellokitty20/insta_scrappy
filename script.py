5eca4ce520d9faf7b46b851fe1090d3abf072b29
bunbo_tainam <macbookpro@Slave-Slx.local>
2021-07-06 13:54:45 +0700

    Messy update

--- script.py
+++ script.py
@@ -5,76 +5,26 @@
 #%% 
 import pandas as pd 
 import os 
-import sys 
 import glob
 from pathlib import Path        
-import shlex
-import subprocess
 from download import download
 #%% 
 '''
     Setting variables and stuff 
 '''
-username = "astro.clo.vn"
-pw = "tuanyeungan04"
-target = "astro.clo.vn"
-'''
-username = "-l astro.clo.vn"
-pw = "-p tuanyeungan04"
-target = "astro.clo.vn"
-'''
-'''
+username = ""
+pw = ""
+target = ""
 os.environ.copy()
 os.environ["username"] = username 
 os.environ["pw"] = pw   
 os.environ["target"] =  target
-'''
 #%%
 '''
     Run this shell if first time runner 
     TODO : Develope this feature instead of using ipython , 
     this allow the script to run on a server 
 '''
-#cmd = f"instaloader --l %s -p %s %s --filename-pattern=%s" %(username,pw,target,"{date_utc}")
-cmd = f"instaloader %s %s %s --filename-pattern=%s" %(username,pw,target,"{date_utc}")
-cmd = ["instaloader"]
-cmd.append(username)
-cmd.append(pw)
-cmd.append(target)
-cmd.append("--filename-pattern={date_utc}")
-print(cmd) 
-#%%
-#This will allow to check for continous status off the cmd 
-process = subprocess.Popen(cmd, 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-
-while True:
-    output = process.stdout.readline()
-    print(output.strip())
-    # Do something else
-    return_code = process.poll()
-    if return_code is not None:
-        print('RETURN CODE', return_code)
-        # Process has finished, read rest of the output 
-        for output in process.stdout.readlines():
-            print(output.strip())
-        break
-'''
-while True:
-    output = process.stdout.readline()
-    print(output.strip())
-    # Do something else
-    return_code = process.poll()
-    if return_code is not None:
-        print('RETURN CODE', return_code)
-        # Process has finished, read rest of the output 
-        for output in process.stdout.readlines():
-            print(output.strip())
-        break 
-'''
-#%%
-download(username)
 #%%
 # Use instaloader to download all picture 
 ! instaloader --l $username  -p $pw $target --filename-pattern={date_utc} 
@@ -90,12 +40,11 @@ download(username)
 '''
 
 def get_path(): 
-    cap_path= "./astro.clo.vn/*.txt" 
+    cap_path= f"./%s/*.txt" %(target)
     #cap_paths = [f for f in glob.glob(cap_path)]
     cap_paths = []
-    img_path= "./astro.clo.vn/*_1.jpg" 
+    img_path= "./%s/*_1.jpg"%(target) 
     img_paths = []
-    none = []
     #Sort the time only add in array if time match  
     for f in glob.glob(img_path):
         c = Path(f).stem[:-2]
@@ -108,8 +57,6 @@ def get_path():
                 continue 
     return cap_paths, img_paths  
 path = get_path()
-print(len(path[0]))
-print(len(path[1]))
 
 #%% 
 '''
@@ -129,7 +76,6 @@ for i in  path[0]:
     name = product_name_search(i)
     product_names.append(name.strip("\n").strip("\n").strip("/"))
 print(product_names)
-
 
 #%%
 '''
